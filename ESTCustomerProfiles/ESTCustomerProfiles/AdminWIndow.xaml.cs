fa53d38bd672ca4d01fd5227b3b47c07bdf80de3
ajitha <ajitha.samarasinghe@gmail.com>
2014-01-20 10:29:33 +0530

    sass
    sss

--- ESTCustomerProfiles/ESTCustomerProfiles/AdminWIndow.xaml.cs
+++ ESTCustomerProfiles/ESTCustomerProfiles/AdminWIndow.xaml.cs
@@ -10,21 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ESTCustomerProfiles
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AdminWIndow.xaml
     /// </summary>
-    /// 
-
-
-    ///
-    public partial class MainWindow : Window
+    public partial class AdminWIndow : Window
     {
-        public MainWindow()
+        public AdminWIndow()
         {
             InitializeComponent();
         }
